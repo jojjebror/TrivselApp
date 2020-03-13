2cb159d5c6b8fb7fe7d754aeec5d81b621cd11fe
Johan Carlsson <jjohaan@live.se>
2020-03-13 16:02:55 +0100

    styleChangeDrinks

--- app/src/app/drink/components/drink-list/drink-list.component.ts
+++ app/src/app/drink/components/drink-list/drink-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Drink } from '../../../shared/models';
 
 @Component({
   selector: 'ex-drink-list',
   templateUrl: './drink-list.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
- // styleUrls: ['./drink-list.component.scss']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./drink-list.component.scss']
   
 })
 export class DrinkListComponent {
