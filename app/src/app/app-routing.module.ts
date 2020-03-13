99ce1133566454b0fbb942c58a180e8e4aff7528
Andre Famili <andrefamili93@gmail.com>
2020-03-13 10:26:27 +0100

    SmallFixes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,46 +4,41 @@ import { RouterModule, PreloadAllModules, Routes } from '@angular/router';
 import { ProtectedComponent } from './core/views';
 import { AuthenticationGuard } from './core/guards';
 
-export const routes: Routes = 
-	[
-		{
-			path: 'authentication',
-			loadChildren: './authentication/authentication.module#AuthenticationModule'
-		},
-		{
-			path: 'login',
-			loadChildren: './login/login.module#LoginModule'
-		},
-		{
-			path: '',
-			component: ProtectedComponent,
-			canActivate: [
-				AuthenticationGuard
-			],
-			canActivateChild: [
-				AuthenticationGuard
-			],
-			children: [
-				{
-					path: 'start',
-					loadChildren: './start/start.module#StartModule'
-				},
-				{
-					path: 'exempel',
-					loadChildren: './example/example.module#ExampleModule'
-				},
-				{
-					path: 'event',
-					loadChildren: './event/event.module#EventModule'
-				},
-				{
-					path: '',
-					redirectTo: 'start',
-					pathMatch: 'full'
-				}
-			]
-		}
-	];
+export const routes: Routes = [
+         {
+           path: 'authentication',
+           loadChildren: './authentication/authentication.module#AuthenticationModule'
+         },
+         {
+           path: 'login',
+           loadChildren: './login/login.module#LoginModule'
+         },
+         {
+           path: '',
+           component: ProtectedComponent,
+           canActivate: [AuthenticationGuard],
+           canActivateChild: [AuthenticationGuard],
+           children: [
+             {
+               path: 'start',
+               loadChildren: './start/start.module#StartModule'
+             },
+             {
+               path: 'exempel',
+               loadChildren: './example/example.module#ExampleModule'
+             },
+             {
+               path: 'event',
+               loadChildren: './event/event.module#EventModule'
+             },
+             {
+               path: '',
+               redirectTo: 'start',
+               pathMatch: 'full'
+             }
+           ]
+         }
+       ];
 
 @NgModule({
 	imports: [
--- app/src/app/event/event-routing.module.ts
+++ app/src/app/event/event-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EventComponent } from './views';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: EventComponent
+    component: EventComponent,
+    children: [
+      {
+        path: '1',
+        component: EventDetailComponent
+      }
+    ]
   }
 ];
 
