681df685d7eeb459ba6defb1eaf96d783c92cec0
Johan Carlsson <jjohaan@live.se>
2020-03-18 09:23:59 +0100

    merge

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,47 +4,6 @@ import { RouterModule, PreloadAllModules, Routes } from '@angular/router';
 import { ProtectedComponent } from './core/views';
 import { AuthenticationGuard } from './core/guards';
 
-<<<<<<< HEAD
-export const routes: Routes = [
-         {
-           path: 'authentication',
-           loadChildren: './authentication/authentication.module#AuthenticationModule'
-         },
-         {
-           path: 'login',
-           loadChildren: './login/login.module#LoginModule'
-         },
-         {
-           path: '',
-           component: ProtectedComponent,
-           canActivate: [AuthenticationGuard],
-           canActivateChild: [AuthenticationGuard],
-           children: [
-             {
-               path: 'start',
-               loadChildren: './start/start.module#StartModule'
-             },
-             {
-               path: 'exempel',
-               loadChildren: './example/example.module#ExampleModule'
-             },
-             {
-               path: 'event',
-               loadChildren: './event/event.module#EventModule'
-             },
-             {
-              path: 'drink',
-              loadChildren: './drink/drink.module#DrinkModule'
-            },
-             {
-               path: '',
-               redirectTo: 'start',
-               pathMatch: 'full'
-             }
-           ]
-         }
-       ];
-=======
 export const routes: Routes = 
 	[
 		{
@@ -94,7 +53,6 @@ export const routes: Routes =
 			]
 		}
 	];
->>>>>>> ccda7fc5b7403c031c2e8f1b6c37467e56dd43e0
 
 @NgModule({
 	imports: [
