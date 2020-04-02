e601e3298b706eaab79440c2f439db834e2aae1a
Johan Carlsson <jjohaan@live.se>
2020-04-02 17:21:05 +0200

    workin

--- app/src/app/drink/state/drinks/drinks.effects.ts
+++ app/src/app/drink/state/drinks/drinks.effects.ts
@@ -1,30 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Action } from '@ngrx/store';
-import { Effect, Actions, ofType } from '@ngrx/effects';
-import { Observable, of } from 'rxjs';
-import { switchMap, map, mergeMap, catchError, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Action } from "@ngrx/store";
+import { Effect, Actions, ofType } from "@ngrx/effects";
+import { Observable, of } from "rxjs";
+import { switchMap, map, mergeMap, catchError, tap } from "rxjs/operators";
 
-
-
-import * as drinksActions from './drinks.actions';
-import { DrinkResource } from 'src/app/core/resources/drink.resource';
-import { Drink } from 'src/app/shared/models';
-import { text } from '@angular/core/src/render3';
-
+import * as drinksActions from "./drinks.actions";
+import { DrinkResource } from "src/app/core/resources/drink.resource";
+import { Drink } from "src/app/shared/models";
+import { text } from "@angular/core/src/render3";
 
 @Injectable()
 export class DrinksEffects {
   router: any;
-  constructor(private actions$: Actions, private drinkResource: DrinkResource) {}
+  constructor(
+    private actions$: Actions,
+    private drinkResource: DrinkResource
+  ) {}
 
   //load drinks
   @Effect()
   loadDrinks$: Observable<Action> = this.actions$.pipe(
     ofType<drinksActions.LoadDrinks>(drinksActions.ActionTypes.LOAD_DRINKS),
     mergeMap((actions: drinksActions.LoadDrinks) =>
-    this.drinkResource.loadDrinks().pipe(
-      map((drinks: Drink[]) => new drinksActions.LoadDrinksSuccess(drinks)),
-      catchError(err => of(new drinksActions.LoadDrinkError(err)))
+      this.drinkResource.loadDrinks().pipe(
+        map((drinks: Drink[]) => new drinksActions.LoadDrinksSuccess(drinks)),
+        catchError(err => of(new drinksActions.LoadDrinkError(err)))
       )
     )
   );
@@ -41,26 +41,25 @@ export class DrinksEffects {
     )
   );
 
-        // create new drink
+  // create new drink
   @Effect()
   createDrink$: Observable<Action> = this.actions$.pipe(
     ofType<drinksActions.CreateDrink>(drinksActions.ActionTypes.CREATE_DRINK),
     map((action: drinksActions.CreateDrink) => action.payload),
     mergeMap((drink: Drink) =>
       this.drinkResource.create(drink).pipe(
-        map((newDrink: Drink) => new drinksActions.CreateDrinkSuccess(newDrink)),
+        map(
+          (newDrink: Drink) => new drinksActions.CreateDrinkSuccess(newDrink)
+        ),
         catchError(err => of(new drinksActions.CreateDrinkError(err)))
       )
     )
   );
 
-  
-        // delete drink
+  // delete drink
   @Effect()
-  deleteDrink$ : Observable<Action> = this.actions$.pipe(
-    ofType<drinksActions.DeleteDrink>(
-      drinksActions.ActionTypes.DELETE_DRINK
-    ),
+  deleteDrink$: Observable<Action> = this.actions$.pipe(
+    ofType<drinksActions.DeleteDrink>(drinksActions.ActionTypes.DELETE_DRINK),
     map((action: drinksActions.DeleteDrink) => action.payload),
     mergeMap((id: number) =>
       this.drinkResource.deleteDrink(id).pipe(
@@ -69,7 +68,7 @@ export class DrinksEffects {
       )
     )
   );
-   // update Drink
+  // update Drink
   @Effect()
   updateDrink$: Observable<Action> = this.actions$.pipe(
     ofType<drinksActions.UpdateDrink>(drinksActions.ActionTypes.UPDATE_DRINK),
@@ -88,20 +87,18 @@ export class DrinksEffects {
     )
   );
 
-/*
   @Effect()
-  category$ : Observable<Action> = this.actions$.pipe(
-    ofType<drinksActions.LoadDrinkCategory>(
-      drinksActions.ActionTypes.LOAD_DRINK_Category
-    ),
-    map((action: drinksActions.LoadDrinkCategory) => action.payload),
-    mergeMap((text: string) =>
-      this.drinkResource.category(text).pipe(
-        map(() => new drinksActions.LoadDrinkCategorySuccess(text)),
-        tap(() => this.router.navigate(['/drink/' + text])),
-        catchError(err => of(new drinksActions.LoadDrinkCategoryError(err)))
+  filterDrink$: Observable<Action> = this.actions$.pipe(
+    ofType<drinksActions.FilterDrink>(drinksActions.ActionTypes.FILTER_DRINK),
+    map((action: drinksActions.FilterDrink) => action.payload),
+    mergeMap((drink: string) =>
+      this.drinkResource.filterDrink(drink).pipe(
+        map(
+          (FilterDrink: Drink[]) =>
+            new drinksActions.FilterDrinkSuccess(FilterDrink)
+        ),
+        catchError(err => of(new drinksActions.FilterDrinkError(err)))
       )
     )
-  ); */
-
+  );
 }
