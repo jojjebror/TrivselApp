e4791e5eb290e1b58bb6963686dbe3ef43701226
martinloord <43519485+martinloord@users.noreply.github.com>
2020-05-12 11:31:01 +0200

    loader on payCredit

--- app/src/app/drink/components/drink-pay-credit/drink-pay-credit.component.ts
+++ app/src/app/drink/components/drink-pay-credit/drink-pay-credit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppState } from 'src/app/core/state';
-import { Store, ActionsSubject } from '@ngrx/store';
+import { Store, ActionsSubject, select } from '@ngrx/store';
 import * as fromUser from '../../../user/state/users/users.actions';
 import * as fromSession from '../../../core/state/session'
 import { MatSnackBar } from '@angular/material';
@@ -9,6 +9,7 @@ import { filter } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/core/services';
 import { Router } from '@angular/router';
 import { User } from 'src/app/shared/models';
+import { getLoadingData } from 'src/app/core/state/loading';
 
 @Component({
   selector: 'ex-drink-pay-credit',
@@ -21,6 +22,7 @@ export class DrinkPayCreditComponent implements OnInit, OnDestroy {
   amount: number;
   userCredit: number;
   user: User;
+  loadings$ = this.store$.pipe(select(getLoadingData));
   
   constructor(private store$: Store<AppState>, private snackBar: MatSnackBar, private router: Router,
   private actionsSubject$: ActionsSubject, public authService: AuthenticationService,) {
